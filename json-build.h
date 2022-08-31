c8c5998eaff86d16ef7b34ea031fac1eb6550ce1
Aníbal Portero Hermida <anibal.portero@pantacor.com>
2022-08-31 07:53:01 +0200

    fix: passing complete buf size argument to _jsonb_escape

--- json-build.h
+++ json-build.h
@@ -272,8 +272,7 @@ jsonb_init(jsonb *b)
 }
 
 JSONB_API jsonbcode
-jsonb_object(jsonb *b, char buf[], size_t bufsize)
-{
+jsonb_object(jsonb *b, char buf[], size_t bufsize) {
     enum jsonbstate new_state;
     size_t pos = 0;
     if (b->top - b->stack >= JSONB_MAX_DEPTH) return JSONB_ERROR_STACK;
@@ -400,7 +399,7 @@ jsonb_key(jsonb *b, char buf[], size_t bufsize, const char key[], size_t len)
     case JSONB_OBJECT_KEY_OR_CLOSE: {
         enum jsonbcode ret;
         BUFFER_COPY_CHAR(b, '"', pos, buf, bufsize);
-        ret = _jsonb_escape(&pos, buf + b->pos, bufsize, key, len);
+        ret = _jsonb_escape(&pos, buf + b->pos, bufsize - b->pos, key, len);
         if (ret != JSONB_OK) return ret;
         BUFFER_COPY(b, "\":", 2, pos, buf, bufsize);
         STACK_HEAD(b, JSONB_OBJECT_VALUE);
@@ -551,7 +550,7 @@ jsonb_string(
         return JSONB_ERROR_INPUT;
     }
     BUFFER_COPY_CHAR(b, '"', pos, buf, bufsize);
-    ret = _jsonb_escape(&pos, buf + b->pos, bufsize, str, len);
+    ret = _jsonb_escape(&pos, buf + b->pos, bufsize - b->pos, str, len);
     if (ret != JSONB_OK) return ret;
     BUFFER_COPY_CHAR(b, '"', pos, buf, bufsize);
     STACK_HEAD(b, next_state);
